0f79087bd0773249addaf223c5ef444e4ee68662
tuyendht <tuyendht@jits.com.vn>
2025-06-30 17:07:11 +0700

    upt log

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 );
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/firebase-messaging-sw.js');
-  });
+  navigator.serviceWorker
+    .register('/firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('✅ Service Worker đăng ký thành công');
+    })
+    .catch((err) => {
+      console.error('❌ Service Worker đăng ký thất bại:', err);
+    });
 }
